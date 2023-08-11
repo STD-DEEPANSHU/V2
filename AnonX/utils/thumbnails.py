07ad5d09afbc0e6fd81480674e62bdd1191108f7
STD DEEPANSHU <108056384+STD-DEEPANSHU@users.noreply.github.com>
2023-08-11 14:40:14 +0530

    Update thumbnails.py

--- AnonX/utils/thumbnails.py
+++ AnonX/utils/thumbnails.py
@@ -124,7 +124,7 @@ async def gen_thumb(videoid, user_id):
         para = textwrap.wrap(title, width=32)
         try:
             draw.text(
-                (460, 35),
+                (460, 20),
                 f"STARTED PLAYING",
                 fill="white",
                 stroke_width=3,

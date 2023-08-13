7c4dfa76cca8b4c8a1c318353920069f47ab8458
STD DEEPANSHU <108056384+STD-DEEPANSHU@users.noreply.github.com>
2023-08-13 08:42:03 +0530

    Update thumbnails.py

--- AnonX/utils/thumbnails.py
+++ AnonX/utils/thumbnails.py
@@ -109,11 +109,11 @@ async def gen_thumb(videoid, user_id):
 
         crop_img = Image.open(f"cache/cropped{videoid}.png")
         logo = crop_img.convert("RGBA")
-        logo.thumbnail((560, 560), Image.LANCZOS)
-        width = int((1280 - 560) / 1)
+        logo.thumbnail((1, 1), Image.LANCZOS)
+        width = int((1280 - 1) / 1)
         background = Image.open(f"cache/temp{videoid}.png")
         background.paste(logo, (width + 1, 45), mask=logo)
-        background.paste(x, (710, 520), mask=x)
+        background.paste(x, (710, 100), mask=x)
         background.paste(image3, (0, 0), mask=image3)
 
         draw = ImageDraw.Draw(background)
@@ -134,7 +134,7 @@ async def gen_thumb(videoid, user_id):
             if para[0]:
                 text_w, text_h = draw.textsize(f"{para[0]}", font=font)
                 draw.text(
-                    ((1280 - text_w) / 3.50, 100),
+                    ((1280 - text_w) / 3.50, 700),
                     f"{para[0]}",
                     fill="white",
                     stroke_width=1,
@@ -144,7 +144,7 @@ async def gen_thumb(videoid, user_id):
             if para[1]:
                 text_w, text_h = draw.textsize(f"{para[1]}", font=font)
                 draw.text(
-                    ((1280 - text_w) / 3.50, 150),
+                    ((1280 - text_w) / 3.50, 750),
                     f"{para[1]}",
                     fill="white",
                     stroke_width=1,
@@ -155,8 +155,8 @@ async def gen_thumb(videoid, user_id):
             pass
         text_w, text_h = draw.textsize(f"Duration: {duration} Mins", font=arial)
         draw.text(
-            ((1280 - text_w) / 2, 660),
-            f"Duration: {duration} Mins",
+            ((1280 - text_w) / 1, 660),
+            f"{duration}",
             fill="white",
             font=arial,
         )

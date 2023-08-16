ca325692be106fcca0baf587b5d64d65d519448b
STD DEEPANSHU <108056384+STD-DEEPANSHU@users.noreply.github.com>
2023-08-16 11:25:41 +0530

    Update thumbnails.py

--- AnonX/utils/thumbnails.py
+++ AnonX/utils/thumbnails.py
@@ -113,7 +113,7 @@ async def gen_thumb(videoid, user_id):
         width = int((1280 - 1) / 1)
         background = Image.open(f"cache/temp{videoid}.png")
         background.paste(logo, (width + 1, 45), mask=logo)
-        background.paste(x, (950, 250), mask=x)
+        background.paste(x, (980, 250), mask=x)
         background.paste(image3, (0, 0), mask=image3)
 
         draw = ImageDraw.Draw(background)
@@ -155,8 +155,8 @@ async def gen_thumb(videoid, user_id):
             pass
         text_w, text_h = draw.textsize(f"Duration: {duration} Mins", font=arial)
         draw.text(
-            ((1280 - 180) / 1, 680),
-            f"                  {duration}",
+            ((1280 - 200) / 1, 680),
+            f"                {duration}",
             fill="white",
             font=arial,
         )
